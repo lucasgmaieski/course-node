4ffe27da9d5b79cbf37dad241c6100115968223b
lucasgmsk <lucasgrigol@gmail.com>
2024-12-28 19:31:54 -0300

    first commit

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,8 @@
+export function buildRoutePath(path) {
+    const routeParametersPath = /:([a-zA-Z]+)/g
+    const pathWithParams = path.replaceAll(routeParametersPath, '(?<$1>[a-z0-9\-_]+)')
+
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
+
+    return pathRegex;
+}
